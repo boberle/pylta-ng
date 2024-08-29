69ff329759091e0264c82778fd0e5194c9bb81b2
Bruno Oberle <bruno@boberle.com>
2024-08-29 11:02:25 +0200

    fastapi: add cors

--- lta/api/app.py
+++ lta/api/app.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 import lta.api.backoffice.endpoints
+from lta.api.configuration import get_allowed_origins
 
 app = FastAPI(openapi_url=None)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=get_allowed_origins(),
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(lta.api.backoffice.endpoints.router, prefix="/api/v1")
