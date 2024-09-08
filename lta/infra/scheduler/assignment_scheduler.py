79ad405a7fdc4ecefede744667bd1641e9f801c0
Bruno Oberle <bruno@boberle.com>
2024-09-08 21:30:38 +0200

    assignment scheduler: add cloud tasks implementation

--- lta/infra/scheduler/assignment_scheduler.py
+++ lta/infra/scheduler/assignment_scheduler.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 from lta.domain.scheduler.assignment_scheduler import AssignmentScheduler
 from lta.domain.scheduler.assignment_service import AssignmentService
+from lta.infra.tasks_api import CloudTasksAPI
 
 
 @dataclass
@@ -12,4 +13,16 @@ class DirectAssignmentScheduler(AssignmentScheduler):
     def schedule_assignment(self, user_id: str, survey_id: str, when: datetime) -> None:
         self.assignment_service.create_assignment(
             user_id=user_id, survey_id=survey_id, ref_time=when
+        )
+
+
+@dataclass
+class CloudTasksAssignmentScheduler(AssignmentScheduler):
+    tasks_api: CloudTasksAPI
+
+    def schedule_assignment(self, user_id: str, survey_id: str, when: datetime) -> None:
+        self.tasks_api.create_task(
+            payload=dict(user_id=user_id, survey_id=survey_id),
+            when=when,
+            task_id=CloudTasksAPI.generate_task_id(user_id, survey_id, when),
         )
