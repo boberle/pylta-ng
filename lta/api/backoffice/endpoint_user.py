888338560df54c6a8ac7c506591141cba4c560ef
Bruno Oberle <bruno@boberle.com>
2024-09-22 17:11:41 +0200

    backoffice: add get user assignments endpoint

--- lta/api/backoffice/endpoint_user.py
+++ lta/api/backoffice/endpoint_user.py
@@ -7,6 +7,7 @@ from pydantic import BaseModel, EmailStr
 
 from lta.api.configuration import AppConfiguration, get_configuration
 from lta.authentication import get_admin_user
+from lta.domain.assignment import Assignment
 from lta.domain.user import DeviceOS, User
 from lta.domain.user_repository import UserNotFound
 
@@ -79,9 +80,51 @@ class UserResponse(BaseModel):
 async def get_user(
     id: str,
     configuration: AppConfiguration = Depends(get_configuration),
+    admin_id: str = Depends(get_admin_user),
 ) -> UserResponse:
     try:
         user = configuration.user_repository.get_user(id)
     except UserNotFound:
         raise HTTPException(status_code=404)
     return UserResponse.from_domain(user)
+
+
+class UserAssignmentItemResponse(BaseModel):
+    id: str
+    title: str
+    created_at: datetime
+    opened_at: datetime | None
+    submitted_at: datetime | None
+
+    @staticmethod
+    def from_domain(assignment: Assignment) -> UserAssignmentItemResponse:
+        print("BAR:", assignment.submitted_at)
+        return UserAssignmentItemResponse(
+            id=assignment.id,
+            title=assignment.title,
+            created_at=assignment.created_at,
+            opened_at=max(assignment.opened_at) if assignment.opened_at else None,
+            submitted_at=assignment.submitted_at,
+        )
+
+
+class UserAssignmentListResponse(BaseModel):
+    assignments: list[UserAssignmentItemResponse]
+
+
+@router.get("/users/{id:str}/assignments/")
+async def get_user_assignments(
+    id: str,
+    configuration: AppConfiguration = Depends(get_configuration),
+    admin_id: str = Depends(get_admin_user),
+) -> UserAssignmentListResponse:
+    try:
+        assignments = configuration.assignment_repository.list_assignments(id)
+    except UserNotFound:
+        raise HTTPException(status_code=404)
+    return UserAssignmentListResponse(
+        assignments=[
+            UserAssignmentItemResponse.from_domain(assignment)
+            for assignment in assignments
+        ]
+    )
