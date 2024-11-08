3125ba008db96ed18e669a63f765daaabc032294
Bruno Oberle <bruno@boberle.com>
2024-11-08 21:04:18 +0100

    cli (local run): add command to schedule assignment

--- lta/cli/local_run.py
+++ lta/cli/local_run.py
@@ -1,9 +1,15 @@
 import json
+from datetime import datetime, timezone
 from pathlib import Path
 
 from typer import Option, Typer
 
-from lta.api.configuration import Environment, get_survey_repository, set_environment
+from lta.api.configuration import (
+    Environment,
+    get_assignment_service,
+    get_survey_repository,
+    set_environment,
+)
 from lta.domain.survey_repository import SurveyCreation
 
 app = Typer()
@@ -28,6 +34,19 @@ def list_surveys() -> None:
     survey_repository = get_survey_repository()
     for survey in survey_repository.list_surveys():
         print(survey)
+
+
+@app.command()
+def schedule_assignment(
+    user_id: str = Option(...),
+    survey_id: str = Option(...),
+) -> None:
+    set_environment(Environment.LOCAL_DEV)
+    assignment_service = get_assignment_service()
+    ref_time = datetime.now(tz=timezone.utc)
+    assignment_service.create_assignment(
+        user_id=user_id, survey_id=survey_id, ref_time=ref_time
+    )
 
 
 if __name__ == "__main__":
